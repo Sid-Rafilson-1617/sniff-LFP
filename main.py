e17eeb5810c1eafc1fbf7a7cb7b5cd3098259822
Sid Rafilson <srafilson@gmail.com>
2024-01-17 17:26:24 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,63 +2,24 @@
 Main run file for local field potential project
 
 By: Sid Rafilson
+Primary Investigator: Matt Smear
 '''
 
-from snifflocklfp import *
-
-sniff_file = r"\\F-MOVING-DATA\EphysData\DATA_MATT_DRIVE\fromData_Restrx_Ob_Final\4122\5\121620_4122_hf2fm_ADC_int16_med0_nch8.bin"
-ephys_file = r"\\F-MOVING-DATA\EphysData\DATA_MATT_DRIVE\fromData_Restrx_Ob_Final\4122\5\121620_4122_hf2fm_Ephys_int16_med0_nch16.bin"
-
-def main():
-
-    print('loading data...')
-    sniff = load_sniff(sniff_file, 30000000)
-    ephys = load_ephys(ephys_file, 30000000, nchannels= 16)
-
-    print('converting data type...')
-    sniff = sniff.astype(np.int32)
-    ephys = ephys.astype(np.int32)
-
-    print('removing artifact...')
-    ephys = remove_jumps_ephys(ephys)
-    sniff = remove_jumps_sniff(sniff)
-
-    print('resampling...')
-    sniff = resample_sniff(sniff)
-    ephys = resample_ephys(ephys, nchannels=16)
-
-    print('finding peaks...')
-    inhales, _ = find_inhales(sniff)
-
-    print('aligning/sorting...')
-    beg = 20000
-    nsniffs = 500
-    window_size = 1000
-    sniff_activity, locs_set = sniff_lock_lfp(inhales, ephys, window_size=window_size, beg = beg, nsniffs = nsniffs)
-    sorted_activity =  sort_lfp(sniff_activity, locs_set)
-
-    print('creating null distributions...')
-    shifts = 100
-    circular_ephys = circular_shift(ephys, shifts)
-    nulls = create_circular_null(circular_ephys, inhales, nsniffs = nsniffs, window_size = window_size, beg = beg)
-
-    print('checking normallity')
-    sig_ratios = plot_normality(nulls, niters = 1000)
-    
-
-    print('finding z-scores from null')
-    sniff_activity_shift, locs_set_1 = sniff_lock_std(inhales, nulls, window_size=window_size, beg = beg, nsniffs = nsniffs)
-    sorted_activity_shift = sort_lfp(sniff_activity_shift, locs_set_1)
-
-
-    print('plotting...')
-    plot_snifflocked_lfp(sorted_activity, subtitle = 'zscored from signal')
-    plot_snifflocked_lfp(sorted_activity_shift, subtitle = 'zscored from null distribution')
-    
-
-    
-    
+from analysis import *
+import numpy as np
 
 
 
-main()
+# file paths
+
+sniff_file = r"E:\Sid_LFP\data\052921_4127_session4_ADC_int16_med0_nch8.bin"
+ephys_file = r"E:\Sid_LFP\data\052921_4127_session4_Ephys_int16_med0_nch16.bin"
+tracking_file = r"\\F-MOVING-DATA\EphysData\rhythmReTrackISH\keypoint_processing\KeypointMatrix\4127\4_keypoints.csv"
+
+
+if __name__ == '__main__':
+
+    ephys, sniff = preprocess(
+        sniff_file, ephys_file, tracking_file,
+          num_samples = -1, start = 0, channels = (1,2),
+            stop = 0, nchannels = 16, remove_artifacts = True, resample = True, no_load = 'tracking')
